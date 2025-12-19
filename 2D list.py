a6571c962cbb9b8749935f98e9da9480c9730e0e
pavithragaddam29-bit <pavithragaddam29@gmail.com>
2025-12-19 12:03:58 +0530

    delete some files

--- 2D list.py
+++
@@ -1,8 +0,0 @@
- # 2D list -students in a section
-secA=[ "priya,sandhya,lilly"]
-secB=[" sweety,bhanu,poojitha"]
-
-class = [ secA,secB] # creating 2D list
-print(class)
-print(class[0])
-print(class[0][1])
--- text read.py
+++
@@ -1,4 +0,0 @@
-file=open("example.txt","r")
-file.write(" hello team")
-
-file.close()
